19ea867c96cd00a9bd2f53b769e8dae6801c35c0
JonasThowsen <jonas.aa.thowsen@gmail.com>
2024-12-12 01:18:23 +0100

    kombinerte TeamList og teams/page til en fil, siden de ikke trenger å være i to filer

--- src/app/teams/page.tsx
+++ src/app/teams/page.tsx
@@ -1,9 +1,25 @@
 import NavBar from "@/components/NavBar";
-import TeamList from "./TeamList";
 import TeamCard from "@/components/TeamCard";
+import { TeamsResponse } from "@/types/team";
+
+async function getTeams() {
+  const response = await fetch(
+    "https://api.football-data.org/v4/competitions/PL/teams",
+    {
+      headers: {
+        "X-Auth-Token": process.env.FOOTBALL_API_KEY || "",
+      },
+    },
+  );
+  if (!response.ok) {
+    throw new Error(`Failed to fetch teams`);
+  }
+  const data: TeamsResponse = await response.json();
+  return data.teams;
+}
 
 export default async function Teams() {
-  const { teams } = await TeamList();
+  const teams = await getTeams();
 
   return (
     <div>
